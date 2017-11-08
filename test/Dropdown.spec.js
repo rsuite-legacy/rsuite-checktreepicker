24f25d08ae09098396830eadabd09270502331c6
Superman <supermanchc@gmail.com>
2017-11-08 18:23:13 +0800

    Add test suite

--- test/Dropdown.spec.js
+++ test/Dropdown.spec.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import Picker from '../src';
 import treeData from '../docs/data/treeData';
+import { delay } from './utils';
 
 const setup = () => {
   const props = {
-    data: treeData
+    data: treeData,
   };
 
   const wrapper = shallow(<Picker {...props} />);
@@ -23,12 +24,29 @@ describe('rsutie-checktrepicker test suite', () => {
   const { wrapper, fullRender, staticRender } = setup();
 
   it('picker should be render, there are class rsuite-checktreepicker-dropdown', () => {
-    expect(wrapper.find('.rsuite-checktreepicker-dropdown'));
+    expect(wrapper.find('.rsuite-checktreepicker-dropdown').length).toBe(1);
   });
 
   it('Picker placeholder text should be Please Select', () => {
     const text = staticRender.find('.placeholder-text > span').text();
     expect(text).toBe('Please Select');
+  });
+
+  it('Picker placeholder text should be 20 Selected when props.value = "["Master", "Maya"]"', async () => {
+    const didMount = jest.fn().mockImplementation(values => values);
+    const props = {
+      data: treeData,
+      value: ['Master', 'Maya'],
+      didMount
+    };
+    const picker = mount(<Picker {...props} />);
+    expect(didMount.mock.calls[0][0].length).toBe(20);
+  });
+
+  it('when dropdown clicked, CheckTree Component should be render', () => {
+    fullRender.find('.rsuite-checktreepicker-toggle').simulate('click');
+    expect(fullRender.find('.dropdown').render().find('.tree-view').length).toBe(1);
+
   });
 
 });
